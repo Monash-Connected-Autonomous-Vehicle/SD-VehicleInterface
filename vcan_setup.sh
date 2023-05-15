a38317eaa47983bc3bee4ab86bc796f473cf4a10
Owen Brooks <owen.h.brooks@gmail.com>
2023-05-15 22:23:57 +1000

    Add success/failure messages to the CAN setup scripts

--- vcan_setup.sh
+++ vcan_setup.sh
@@ -1,3 +1,9 @@
-sudo modprobe vcan
-sudo ip link add can0 type vcan 
-sudo ip link set can0 up
+sudo modprobe vcan && sudo ip link add can0 type vcan && sudo ip link set can0 up
+
+# Check if the previous command was successful
+if [ $? -eq 0 ]
+then
+    echo "CAN bus setup successful"
+else
+    echo "Error. CAN bus setup failed. Disconnect and try again"
+fi
