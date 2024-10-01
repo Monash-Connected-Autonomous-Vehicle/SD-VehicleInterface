cdccad164580d2ee09fd8866bdc9b82d351c6516
dtonda8 <davidbatonda7@gmail.com>
2024-10-01 21:16:30 +1000

    Remove invalid text
    - Caused by unresolved merge conflicts

--- vehicle_interface/src/sd_vehicle_interface/sd_vehicle_interface.cpp
+++ vehicle_interface/src/sd_vehicle_interface/sd_vehicle_interface.cpp
@@ -34,10 +34,7 @@ using namespace std;
 #include "rclcpp/rclcpp.hpp"
 #include "sd_msgs/msg/sd_control.hpp"
 #include "autoware_control_msgs/msg/control.hpp"
-<<<<<<< HEAD
 #include "autoware_vehicle_msgs/msg/turn_indicators_report.hpp"
-=======
->>>>>>> 7ee4887 (Update according to renamed autoware topics)
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
@@ -76,11 +73,7 @@ void ReceivedFrameCANRx_callback(const std::shared_ptr<can_msgs::msg::Frame> msg
 }
 
 
-<<<<<<< HEAD
-void AckermannCommand_callback(const std::shared_ptr<autoware_control_msgs::msg::AckermannControlCommand> msg)
-=======
 void AckermannCommand_callback(const std::shared_ptr<autoware_control_msgs::msg::Control> msg)
->>>>>>> 7ee4887 (Update according to renamed autoware topics)
 {
 	//Populate a twist angular and twist linear message with the received message from Ros topic and convert to deg/s
     TargeTireAngle_Rad= msg->lateral.steering_tire_angle; //Radians
@@ -120,11 +113,7 @@ int main(int argc, char **argv)
 	//Subscriber
     auto ReceivedFrameCANRx_sub = node->create_subscription<can_msgs::msg::Frame>("from_can_bus", 100, ReceivedFrameCANRx_callback);
     auto current_velocity_sub = node->create_subscription<geometry_msgs::msg::TwistStamped>("current_velocity", 1, CurrentVelocity_callback);
-<<<<<<< HEAD
-    auto ackermann_cmd_sub = node->create_subscription<autoware_control_msgs::msg::AckermannControlCommand>("/control/command/control_cmd", 100, AckermannCommand_callback);
-=======
     auto ackermann_cmd_sub = node->create_subscription<autoware_control_msgs::msg::Control>("/control/command/control_cmd", 100, AckermannCommand_callback);
->>>>>>> 7ee4887 (Update according to renamed autoware topics)
 
     //publisher
 	auto sent_msgs_pub = node->create_publisher<can_msgs::msg::Frame>("to_can_bus", 100);
