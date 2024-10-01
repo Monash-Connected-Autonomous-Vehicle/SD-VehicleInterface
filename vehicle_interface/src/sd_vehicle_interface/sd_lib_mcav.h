a247f1fdee9e637f76a3cae429b11850e1275f0d
Surya-SAT <165343982+Surya-Sathya@users.noreply.github.com>
2024-10-01 18:07:09 +1000

    Publish Surya's Ackermann Branch with Frame 0x104 Function in sd_lib_mcav.h

--- vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
+++ vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
@@ -98,6 +98,10 @@ namespace sd{
 	This function parses an input can frame, checks it's ID and updates appropriate variables with freshest data*/
     void ParseRxCANDataSDCan(can_msgs::msg::Frame& frame, double& CurrentLinearVelocity_Mps, bool& AutomationArmed_B, bool& AutomationGranted_B) {
 		// Check which kind of frame we have received and update data accordingly
+		if (frame.id == 0x104) { // StreetDrone_Control_1
+			bool Left_Indicator_Command = frame.data[5] & 0x01; // bit 56 (0b0000 0001)
+			bool Right_Indicator_Command = frame.data[5] & 0x40; // bit 57 (0b0100 0000)
+		}
 		if (frame.id == 0x100) { // StreetDrone_Control_1
 			bool steer_automation_available = frame.data[7] & 0x01; // bit 56 (0b0000 0001)
 			bool steer_automation_granted = frame.data[7] & 0x40; // bit 57 (0b0100 0000)
