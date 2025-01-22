c0b95fca724f78b523d1a08958c5be1ec4eaa878
dtonda8 <davidbatonda7@gmail.com>
2025-01-22 19:57:03 +1100

    fix

--- vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
+++ vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
@@ -232,7 +232,7 @@ namespace sd{
         int8_t bit_hazard = (setCommand ? 1 : 0) << 6;
 //        int8_t bit_horn = (setCommand ? 1 : 0) << 7;
 
-		frame.data[5] = bit_main_beam | bit_dip_lights | bit_side_lights | bit_ric | bit_lic | bit_hazard | bit_horn;
+		frame.data[5] = bit_main_beam | bit_dip_lights | bit_side_lights | bit_ric | bit_lic | bit_hazard;
 	}
 	/*Inputs
 	can_msgs::msg::Frame& CustomerControl2CANTx  The SD Interface Customer Control 2 Message
