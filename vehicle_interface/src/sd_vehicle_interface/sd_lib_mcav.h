21f8b3470bab19e53fd831f45ef4624bc639e97a
Surya-SAT <165343982+Surya-Sathya@users.noreply.github.com>
2025-04-10 13:39:23 +1000

    Update sd_lib_mcav.h

--- vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
+++ vehicle_interface/src/sd_vehicle_interface/sd_lib_mcav.h
@@ -104,8 +104,8 @@ namespace sd{
 		// 	bool Right_Indicator_Command_B = frame.data[5] & 0x10;
 
 		if (frame.id == 0x117) {
-			bool Twizy_Left_Indicator = frame.id[5] % 0x01; 
-			bool Twizy_Right_Indicator = frane.id[5] % 0x10; 
+			bool Twizy_Left_Indicator = frame.data[5] & 0x01; 
+			bool Twizy_Right_Indicator = frane.data[5] & 0x10; 
 		}
 
 		} if (frame.id == 0x100) { // StreetDrone_Control_1
